84cf1e745726826db6eb41e52d1484438aade78f
RabidChinchilla <33025242+RabidChinchilla@users.noreply.github.com>
2017-11-13 13:01:40 +0000

    working thought javascript exercises part 2

--- ClassWork/week 4/Javascript_Examples/Exercise3/Exercise3Javascript.js
+++ ClassWork/week 4/Javascript_Examples/Exercise3/Exercise3Javascript.js
@@ -1,5 +1,10 @@
 var isLightOn = false;
 function ToggleLight()
 {
-	
+	if (isLightOn === true){
+		document.getElementById("lightBulb").src="img/bulb-on.png";
+	}
+	else{
+		document.getElementById("lightBulb").src="img/bulb-off.png";
+	}
 }
