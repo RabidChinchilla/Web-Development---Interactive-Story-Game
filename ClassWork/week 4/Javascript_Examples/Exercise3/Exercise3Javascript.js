7d16f0a667cc7f3722d816dd0274619fc35c5b62
RabidChinchilla <33025242+RabidChinchilla@users.noreply.github.com>
2017-11-13 14:35:02 +0000

    trying to get exercise 3 to work

--- ClassWork/week 4/Javascript_Examples/Exercise3/Exercise3Javascript.js
+++ ClassWork/week 4/Javascript_Examples/Exercise3/Exercise3Javascript.js
@@ -1,10 +1,21 @@
 var isLightOn = false;
 function ToggleLight()
 {
-	if (isLightOn === true){
+	if (isLightOn = false)
+	{
 		document.getElementById("lightBulb").src="img/bulb-on.png";
+		isLightOn = true;
 	}
-	else{
+	else if (isLightOn = true)
+	{
 		document.getElementById("lightBulb").src="img/bulb-off.png";
+		isLightOn = false;
 	}
+}
+
+function AddNumbers(param1, param2)
+{
+	console.log(param1 + param2)
+	answer = param3
+	document.getElementById("numbers").innerHTML = param3;
 }
