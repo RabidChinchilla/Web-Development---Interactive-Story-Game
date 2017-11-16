245ffe75bf1e00eae3d9e48e26040b4b2f63a07f
RabidChinchilla <33025242+RabidChinchilla@users.noreply.github.com>
2017-11-16 09:20:57 +0000

    exercise 3 finished

--- ClassWork/week 4/Javascript_Examples/Exercise3/Exercise3Javascript.js
+++ ClassWork/week 4/Javascript_Examples/Exercise3/Exercise3Javascript.js
@@ -1,10 +1,20 @@
 var isLightOn = false;
 function ToggleLight()
 {
-	if (isLightOn === true){
+	if (isLightOn == false){
 		document.getElementById("lightBulb").src="img/bulb-on.png";
+		isLightOn = true;
 	}
-	else{
+	else if (isLightOn == true){
 		document.getElementById("lightBulb").src="img/bulb-off.png";
+		isLightOn = false;
 	}
+}
+
+function AddNumbers()
+{
+	var x = 10;
+	var y = 20;
+	var ans = x + y;
+	document.getElementById("numbers").innerHTML = ans;
 }
