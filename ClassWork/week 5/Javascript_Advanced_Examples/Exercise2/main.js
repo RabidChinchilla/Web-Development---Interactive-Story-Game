6b2535357e757d9ad639cac5b14fe4685361e93f
RabidChinchilla <33025242+RabidChinchilla@users.noreply.github.com>
2017-11-20 12:59:55 +0000

    attempting exercise 2

--- ClassWork/week 5/Javascript_Advanced_Examples/Exercise2/main.js
+++ ClassWork/week 5/Javascript_Advanced_Examples/Exercise2/main.js
@@ -1,6 +1,17 @@
 
 function StorePlayerData()
 {
-	
+	localStorage.setItem("playerName",document.forms[0]["playername"].value);
+	localStorage.setItem("playerHealth",document.forms[0]["playerhealth"].value);
+	localStorage.setItem("playerColour",document.forms[0]["playercolour"].value);
 }
 
+function OnLoad() {
+	var name = localStorage.getItem("playerName");
+	var health = localStorage.getItem("playerHealth");
+	var colour = localStorage.getItem("playerColour");
+
+	document.getElementById("playername").innerHTML = name;
+	document.getElementById("playerhealth").innerHTML = health;
+	document.getElementById("playercolour").innerHTML = colour;
+}
