c38208cc122d82fc2d51f0a30ea46080e48eaff2
RabidChinchilla <33025242+RabidChinchilla@users.noreply.github.com>
2017-11-20 12:12:46 +0000

    completed exercise 1

--- ClassWork/week 5/Javascript_Advanced_Examples/Exercise1/main.js
+++ ClassWork/week 5/Javascript_Advanced_Examples/Exercise1/main.js
@@ -8,7 +8,8 @@ var weaponsArray = [
 function OnLoad()
 {
 	for(var i = 0; i < weaponsArray.length; i++){
-		var tagWeapon = "<option value='(i + 1)'>"weaponsArray[i]"</option>"
+		var tagWeapon = "<option value='" + i + "'>" + weaponsArray[i] + "</option>";
+		document.forms[0]["weapons"].innerHTML += tagWeapon;
 	}
 }
 
