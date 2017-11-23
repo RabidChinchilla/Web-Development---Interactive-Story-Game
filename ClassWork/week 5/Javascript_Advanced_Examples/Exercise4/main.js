e05902b7936aba48a62dc55270d13526aff1a2fb
RabidChinchilla <33025242+RabidChinchilla@users.noreply.github.com>
2017-11-23 10:14:04 +0000

    completed exercise 4

--- ClassWork/week 5/Javascript_Advanced_Examples/Exercise4/main.js
+++ ClassWork/week 5/Javascript_Advanced_Examples/Exercise4/main.js
@@ -5,5 +5,15 @@ function OnLoad()
 
 function SelectRoom(roomIndex)
 {
-	
+	document.getElementById("roomTitle").innerHTML = roomArray[roomIndex].title;
+	document.getElementById("roomText").innerHTML = roomArray[roomIndex].text;
+
+	document.getElementById("roomChoices").innerHTML = "";
+
+	for(var i = 0; i < roomArray[roomIndex].choices.length; i++)
+	{
+		var options = "<button type='button' onClick='SelectRoom(" + roomArray[roomIndex]["choices"][i]["index"] + ")'>" + roomArray[roomIndex]["choices"][i]["text"] + "</button>";
+		//square brackets specify each item down the list
+		document.getElementById("roomChoices").innerHTML += options;
+	}
 }
